6e96a0505fcd22a3b308a4a51dabef8378cb08d0
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-10-23 16:43:03 +0545

    new

--- Weather App/src/App.jsx
+++ Weather App/src/App.jsx
@@ -1,24 +1,20 @@
-// Project 3: Weather App with API Integration
+// Project: Weather App with API Integration
 // Details: Create a weather app that fetches real-time weather data from an external API (like OpenWeatherMap).
-// Requirements:
-// Input field to enter the city name.
-// Fetch and display temperature, humidity, and weather description.
-// Display loading or error messages while data is being fetched.
-// Style the app based on the weather (e.g., sunny, rainy).
-
-// API key = 8c4d64dbb42567072702bb006c3fc005
-// API call = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 import { useState, useEffect } from "react";
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("Kathmandu");
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   const API_KEY = "8c4d64dbb42567072702bb006c3fc005";
 
+  // Step 3: When value of city changes, it re-renders and fetches the data from API again and again
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
@@ -29,6 +25,8 @@ function App() {
           throw new Error("City not found");
         }
         const data = await response.json();
+
+        // Step 4: When the data is received, 'weatherData' is updated with new data and used to display the weather information
         setWeatherData(data);
         setError(null);
       } catch (error) {
@@ -38,31 +36,48 @@ function App() {
 
     fetchWeatherData();
     console.log(weatherData);
-
   }, [city]);
 
-  const handleChange = (e) => {
-    setCity(e.target.value);
+  // Step 2: The data obtained from the form updates the value of city and causes useEffect to re-render
+  const onSubmit = (data) => {
+    console.log(data);
+    setCity(data.city);
   };
 
   return (
     <>
-      <h1>Weather App</h1>
-      <input type="text" value={city} onChange={handleChange} />
+      <div>
 
-      {error && <p style={{color:"red"}}></p>}
+        <h1>Weather App</h1>
 
-      {weatherData && (
+        {/* Step 1: The form is submitted using React Hook Form */}
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="city"
+            {...register("city", { value: "Kathmandu", required: true })}
+          />
 
-        <div>
+          <input type="submit" value="submit" />
+        </form>
+
+        {/* Step 5: If weatherData is obtained, it is displayed, if not, error message is displayed */}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        {weatherData && (
+          <div>
             <p>Location: {weatherData.name}</p>
             <p>Country: {weatherData.sys.country}</p>
 
-            <p>Co-ordinates: <br></br>Longitute: {weatherData.coord.lon} & Latitude: {weatherData.coord.lat}</p>
+            <p>
+              Co-ordinates: <br></br>Longitute: {weatherData.coord.lon} &
+              Latitude: {weatherData.coord.lat}
+            </p>
 
             <p>Temperature: {weatherData.main.temp} &deg;C</p>
-        </div>
-      )}
+          </div>
+        )}
+      </div>
     </>
   );
 }
